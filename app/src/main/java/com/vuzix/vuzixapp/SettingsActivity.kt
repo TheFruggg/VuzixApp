98ed3c9e6fb24b1c2b1560f268a82c5c52ffde11
HamishAlsop <hamish@alsop.net>
2024-02-13 11:51:26 +0000

    13/02/2024

--- app/src/main/java/com/vuzix/vuzixapp/SettingsActivity.kt
+++ app/src/main/java/com/vuzix/vuzixapp/SettingsActivity.kt
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
 
+    // Function called when the activity is created
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Set the content view to the activity_settings layout
         setContentView(R.layout.activity_settings)
     }
 }
